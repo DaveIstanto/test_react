7bad1f05b8f84c681332d41787616d5d33af167b
Dave Istanto <daveistanto@Daves-Machine.local>
2019-11-24 20:57:19 -0600

    Updates

---
+++ precache-manifest.0633f7cec49b9ab9ba0f40f1a24f8c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a01eb2d47a3299c8d8f74632176197b",
+    "url": "/DaveIstanto/test_react/index.html"
+  },
+  {
+    "revision": "4fff0882caa38a8d7f26",
+    "url": "/DaveIstanto/test_react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7b695a6162ac52c3786a",
+    "url": "/DaveIstanto/test_react/static/js/2.36c7cce4.chunk.js"
+  },
+  {
+    "revision": "4fff0882caa38a8d7f26",
+    "url": "/DaveIstanto/test_react/static/js/main.f28d77a2.chunk.js"
+  },
+  {
+    "revision": "3ee597d8ffaf9b21fb2a",
+    "url": "/DaveIstanto/test_react/static/js/runtime-main.fd8ac748.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/DaveIstanto/test_react/static/media/logo.25bf045c.svg"
+  }
+]);
